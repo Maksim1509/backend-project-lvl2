5c3b11ba860273e1a5f0d535121893593e6ff492
maksim1509 <maaa23@mail.ru>
2020-01-05 10:14:46 +0400

    add using __dirname to tests

--- __tests__/__bin__/index.test.js
+++ __tests__/__bin__/index.test.js
@@ -1,17 +1,12 @@
+import fs from 'fs';
+import PATH from 'path';
 import getDiff from '../../src/bin/getDiff';
 
 test('default', () => {
-  const pathToFile1 = './__tests__/__fixtures__/before.json';
-  const pathToFile2 = './__tests__/__fixtures__/after.json';
+  const pathToFile1 = PATH.join(__dirname, '../__fixtures__/before.json');
+  const pathToFile2 = PATH.join(__dirname, '../__fixtures__/after.json');
 
-  const expected = `{
-    host: hexlet.io
-  - timeout: 50
-  + timeout: 20
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
+  const expected = fs.readFileSync(PATH.join(__dirname, '../__fixtures__/expected.txt'), 'utf-8');
 
   const actual = getDiff(pathToFile1, pathToFile2);
   expect(actual).toEqual(expected);

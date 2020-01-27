dba49b63e1bed13bdbfbd0364d9a7e310c6e3686
maksim1509 <maaa23@mail.ru>
2020-01-27 23:03:25 +0400

    refactoring plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import { flatten } from 'lodash';
 
 const stringify = (value) => {
   if (typeof value === 'number' || typeof value === 'boolean') return value;
@@ -11,26 +11,21 @@ const buildKeyLine = (key, item) => {
 };
 
 const buildSting = {
-  add: (keyLine, oldValue, newValue) => `Property '${keyLine}' was added whith value: ${stringify(newValue)}`,
+  add: (keyLine, { newValue }) => `Property '${keyLine}' was added whith value: ${stringify(newValue)}`,
   remove: (keyLine) => `Property '${keyLine}' was removed`,
   changed:
-   (keyLine, oldValue, newValue) => (
+   (keyLine, { oldValue, newValue }) => (
      `Property '${keyLine}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`),
   unchanged: () => null,
-  hasChildren: (keyLine, oldValue, newValue, children, f) => f(children, keyLine),
+  hasChildren: (keyLine, { children }, f) => f(children, keyLine),
 };
 
 const render = (ast, key = '') => {
   const result = ast.map((item) => {
     const keyLine = buildKeyLine(key, item);
-    const {
-      type, children, oldValue, newValue,
-    } = item;
-    // if (type === 'hasChildren') return render(children, keyLine);
-    return buildSting[type](keyLine, oldValue, newValue, children, render);
+    return buildSting[item.type](keyLine, item, render);
   });
-
-  return _.flatten(result);
+  return flatten(result);
 };
 
 export default (ast, key) => render(ast, key).filter((a) => a).join('\n');

074001a7d14a9ec4ad7fcf83ed66eb28b49a2a32
maksim1509 <maaa23@mail.ru>
2020-02-11 19:15:20 +0400

    move getDiff to index.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,8 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import { has, union } from 'lodash';
-import getParse from './parser';
-import render from './formatters';
 
 const getType = [
   {
@@ -50,17 +46,4 @@ const getDifferenceAst = (firstData, secondData) => {
   });
   return difference;
 };
-
-export default (pathToFirstFile, pathToSecondFile, format = 'stylish') => {
-  const firstData = fs.readFileSync(pathToFirstFile, 'utf8');
-  const secondData = fs.readFileSync(pathToSecondFile, 'utf-8');
-
-  const firstDataType = path.extname(pathToFirstFile).slice(1);
-  const secondDataType = path.extname(pathToSecondFile).slice(1);
-
-  const firstParseredData = getParse(firstDataType)(firstData);
-  const secondParseredData = getParse(secondDataType)(secondData);
-
-  const differenceAst = getDifferenceAst(firstParseredData, secondParseredData);
-  return render(differenceAst, format);
-};
+export default getDifferenceAst;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import getDiff from './getDiff';
+import fs from 'fs';
+import path from 'path';
+import getDifferenceAst from './getDiff';
+import getParse from './parser';
+import render from './formatters';
 
-export default getDiff;
+export default (pathToFirstFile, pathToSecondFile, format = 'stylish') => {
+  const firstData = fs.readFileSync(pathToFirstFile, 'utf8');
+  const secondData = fs.readFileSync(pathToSecondFile, 'utf-8');
+
+  const firstDataType = path.extname(pathToFirstFile).slice(1);
+  const secondDataType = path.extname(pathToSecondFile).slice(1);
+
+  const firstParseredData = getParse(firstDataType)(firstData);
+  const secondParseredData = getParse(secondDataType)(secondData);
+
+  const differenceAst = getDifferenceAst(firstParseredData, secondParseredData);
+  return render(differenceAst, format);
+};
